f6fe270d430c7cb373917b0b27eb8c301e4f32c8
AndyZe <andyz@utexas.edu>
2025-07-07 16:40:16 -0500

    PDO variable renaming

--- include/synapticon_ros2_control/synapticon_interface.hpp
+++ include/synapticon_ros2_control/synapticon_interface.hpp
@@ -164,9 +164,9 @@ private:
   OSAL_THREAD_HANDLE ecat_error_thread_;
   char io_map_[4096];
 
-  std::vector<InSomanet50t *> in_somanet_1_;
-  std::mutex in_somanet_mtx_;
-  std::vector<OutSomanet50t *> out_somanet_1_;
+  std::vector<InSomanet50t *> in_somanet_;
+  std::mutex hw_state_mtx_;
+  std::vector<OutSomanet50t *> out_somanet_;
 
   std::vector<uint32_t> encoder_resolutions_;
 
