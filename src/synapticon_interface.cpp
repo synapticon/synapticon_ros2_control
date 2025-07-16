5418987f076f6a6b09403f15e187738446ba8a97
AndyZe <andyz@utexas.edu>
2025-07-16 12:04:53 -0500

    Allow yaw1 joint to be free during compensation, too, for better compliance

--- src/synapticon_interface.cpp
+++ src/synapticon_interface.cpp
@@ -38,6 +38,7 @@ unsigned int NORMAL_OPERATION_BRAKES_OFF = 0b00001111;
 unsigned int NORMAL_OPERATION_BRAKES_ON = 0b00001011;
 constexpr char EXPECTED_SLAVE_NAME[] = "SOMANET";
 constexpr std::array<double, 7> TORQUE_FRICTION_OFFSET = {0, 0, 0, 0, 0, 0, 0}; // per mill
+constexpr size_t YAW_1_IDX = 0;
 constexpr size_t YAW_2_IDX = 1;
 constexpr size_t SPRING_ADJUST_IDX = 2;
 constexpr size_t INERTIAL_ACTUATOR_IDX = 3;
@@ -494,8 +495,8 @@ SynapticonSystemInterface::prepare_command_mode_switch(
           std::lock_guard<std::mutex> lock(hw_state_mtx_);
           initial_inertial_act_position_rad_ = hw_states_positions_[INERTIAL_ACTUATOR_IDX];
         }
-        // compensate_for_added_load puts all joints in QUICK_STOP mode except those in the elevation link
-        if ((i == SPRING_ADJUST_IDX) || (i == INERTIAL_ACTUATOR_IDX) || (i == YAW_2_IDX)) {
+        // compensate_for_added_load puts all joints in QUICK_STOP mode besides these joints
+        if ((i == SPRING_ADJUST_IDX) || (i == INERTIAL_ACTUATOR_IDX) || (i == YAW_1_IDX) || (i == YAW_2_IDX)) {
           new_modes.push_back(control_level_t::COMPENSATE_FOR_ADDED_LOAD);
         } else {
           new_modes.push_back(control_level_t::QUICK_STOP);
@@ -998,9 +999,9 @@ void SynapticonSystemInterface::somanetCyclicLoop(
                 out_somanet_[joint_idx]->OpMode = PROFILE_TORQUE_MODE;
                 out_somanet_[joint_idx]->Controlword = NORMAL_OPERATION_BRAKES_OFF;
               }
-              // Yaw2 joint should be free to move to allow some compliance, i.e.
+              // Yaw joints should be free to move to allow some compliance, i.e.
               // in case the end effector is pushed into the wall
-              else if (joint_idx == YAW_2_IDX) {
+              else if ((joint_idx == YAW_1_IDX) || (joint_idx == YAW_2_IDX)) {
                 out_somanet_[joint_idx]->TargetTorque = 0;
                 out_somanet_[joint_idx]->OpMode = PROFILE_TORQUE_MODE;
                 out_somanet_[joint_idx]->Controlword = NORMAL_OPERATION_BRAKES_OFF;
